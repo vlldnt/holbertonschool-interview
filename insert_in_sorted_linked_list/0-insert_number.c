29cf3d72b716a6a5f4e5cf59db361f4490e94d4b
vlldnt <vieilledent.adrien@gmail.com>
2025-08-06 14:06:06 +0200

    Update: Enhance README.md files and implement linked list functions for insert_node

---
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_node - fucntion to insert a int
+ * @head: list of integer
+ * @number: singly linked list
+ * Return: the address of the new node, or NULL if it failed
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+
+}
