f2b776616b236e4b2ed9641b149e52937bde0aac
vlldnt <vieilledent.adrien@gmail.com>
2025-08-06 11:27:54 +0200

    Add initial implementation of canUnlockAll function and .gitignore file

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+''' method that determines if all the boxes can be opened.'''
+
+
+def canUnlockAll(boxes):
+    if not boxes:
+        return False
+
+    opened = {0}
+    keys = boxes[0]
+
+    while keys:
+        key = keys.pop()
+        if key not in opened and key < len(boxes):
+            opened.add(key)
+
+        for k in boxes[key]:
+            if k not in opened:
+                keys.append(k)
+
+    return len(boxes) == len(opened)
