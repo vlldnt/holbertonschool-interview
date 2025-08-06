517cdbc9ece6dca1b30e5b8fdfd5569f007a8ba0
vlldnt <vieilledent.adrien@gmail.com>
2025-08-06 11:36:50 +0200

    Refactor: canUnlockAll function for improved readability

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -13,9 +13,8 @@ def canUnlockAll(boxes):
         key = keys.pop()
         if key not in opened and key < len(boxes):
             opened.add(key)
-
-        for k in boxes[key]:
-            if k not in opened:
-                keys.append(k)
+            for k in boxes[key]:
+                if k not in opened:
+                    keys.append(k)
 
     return len(boxes) == len(opened)
