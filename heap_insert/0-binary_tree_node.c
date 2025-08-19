fd4b32da69bfaed0be78f4fe8014459c14a99b69
vlldnt <vieilledent.adrien@gmail.com>
2025-08-19 15:15:11 +0200

    fix: correct formatting and comments in binary_tree_node function

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -1,25 +1,25 @@
-#include "binary_trees.h"
 #include <stdlib.h>
+#include "binary_trees.h"
 
 /**
- * *binary_tree_node - creates a binary tree node
- * @parent: Pointer to the parent node
- * @value: Value of the added node
- * Return: Node created
+ * binary_tree_node - creates a binary tree node
+ * @parent: pointer to the parent node
+ * @value: value to store in the new node
+ *
+ * Return: pointer to the created node, or NULL on failure
  */
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *node;
+	binary_tree_t *node;
 
-    node = malloc(sizeof(binary_tree_t));
-    if (node == NULL)
-        return (NULL);
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
 
-    node->n = value;
-    node->parent = parent;
-    node->left = NULL;
-    node-> right = NULL;
+	node->n = value;
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
 
-    return (node);
+	return (node);
 }
