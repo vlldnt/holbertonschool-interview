3501e97520baa0b54f988e00adbb6557b3040a67
vlldnt <vieilledent.adrien@gmail.com>
2025-09-05 14:47:50 +0200

    feat: adding toppling and unstable fiunction

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -1,20 +1,93 @@
-#include <stdlib.h>
+#include "sandpiles.h"
 
 /**
- * sandpiles_sum - adds two 3x3 sandpile grids
- * @grid1: first 3x3 grid
- * @grid2: second 3x3 grid
+ * print_grid - Prints a 3x3 grid
+ * @grid: 3x3 grid
+ *
  */
-
-void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+static void print_grid(int grid[3][3])
 {
 	int i, j;
+
+	printf("=\n");
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+/**
+ * is_unstable - Checks if a grid is unstable
+ * @grid: 3x3 grid
+ * Return: 1 if unstable, 0 if stable
+ */
+static int is_unstable(int grid[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			if (grid[i][j] > 3)
+				return (1);
+
+	return (0);
+}
+
+/**
+ * topple - Topple the grid
+ * @grid: grid
+ */
+static void topple(int grid[3][3])
+{
+	int i, j;
+	int tmp[3][3];
+
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			tmp[i][j] = grid[i][j];
 
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 3; j++)
 		{
-			grid1[i][j] += grid2[i][j];
+			if (tmp[i][j] > 3)
+			{
+				grid[i][j] -= 4;
+				if (i > 0)
+					grid[i - 1][j] += 1;
+				if (i < 2)
+					grid[i + 1][j] += 1;
+				if (j > 0)
+					grid[i][j - 1] += 1;
+				if (j < 2)
+					grid[i][j + 1] += 1;
+			}
 		}
+	}
+}
+
+/**
+ * sandpiles_sum - adds and stabilizes two sandpiles
+ * @grid1: first grid
+ * @grid2: second grid
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			grid1[i][j] += grid2[i][j];
+
+	while (is_unstable(grid1))
+	{
+		print_grid(grid1);
+		topple(grid1);
 	}
 }
--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -1,6 +1,7 @@
 #ifndef SANDPILES_H
 #define SANDPILES_H
 
-void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+#include <stdio.h>
 
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 #endif
