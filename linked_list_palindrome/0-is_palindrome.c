6e5af3697889428bbb80ceced761a8bc23888954
vlldnt <vieilledent.adrien@gmail.com>
2025-09-05 16:15:58 +0200

    feat: implement copy_list function and update is_palindrome logic

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -1,10 +1,8 @@
 #include "lists.h"
-#include <stddef.h>
 
 /**
  * reverse - Reverses a singly linked list
  * @list: Pointer to the first node
- *
  * Return: New head of reversed list
  */
 listint_t *reverse(listint_t *list)
@@ -24,9 +22,37 @@ listint_t *reverse(listint_t *list)
 }
 
 /**
+ * copy_list - Creates a copy of a singly linked list
+ * @head: Pointer to the first node
+ * Return: Head of the copied list
+ */
+listint_t *copy_list(listint_t *head)
+{
+	listint_t *copy = NULL;
+	listint_t *tail = NULL;
+	listint_t *new_node;
+
+	while (head != NULL)
+	{
+		new_node = malloc(sizeof(listint_t));
+		if (!new_node)
+			return (NULL);
+		new_node->n = head->n;
+		new_node->next = NULL;
+		if (!copy)
+			copy = new_node;
+		else
+			tail->next = new_node;
+		tail = new_node;
+		head = head->next;
+	}
+
+	return (copy);
+}
+
+/**
  * is_palindrome - Checks if a singly linked list is a palindrome
  * @head: Pointer to the head pointer of the list
- *
  * Return: 1 if palindrome, 0 if not
  */
 int is_palindrome(listint_t **head)
@@ -38,18 +64,24 @@ int is_palindrome(listint_t **head)
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 
-	reversed_list = reverse(*head);
+	reversed_list = reverse(copy_list(*head));
+	if (!reversed_list)
+		return (0);
+
 	original_list = *head;
 	compare_list = reversed_list;
 
 	while (original_list != NULL && compare_list != NULL)
 	{
 		if (original_list->n != compare_list->n)
+		{
+			free_listint(reversed_list);
 			return (0);
-
+		}
 		original_list = original_list->next;
 		compare_list = compare_list->next;
 	}
 
+	free_listint(reversed_list);
 	return (1);
 }
