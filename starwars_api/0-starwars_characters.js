4aaaeff63817f8e64f1909304a1b3d7ec23e6d5e
vlldnt <vieilledent.adrien@gmail.com>
2025-11-20 11:47:56 +0100

    fix:  var to const

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-var request = require("request");
+const request = require("request");
 
 const movieNumber = process.argv[2];
 
@@ -8,7 +8,7 @@ if (!movieNumber) {
   console.log("Need to write: ./0-starwars_characters.js <Movie ID>");
   process.exit(1);
 }
-
+// Fetch Starwars API with a get for the Movie ID
 const url = `https://swapi-api.hbtn.io/api/films/${movieNumber}/`;
 
 request(url, (error, response, body) => {
@@ -22,8 +22,11 @@ request(url, (error, response, body) => {
   }
 
   const movie = JSON.parse(body);
+
+  // List of URL fetched in the request before
   urlsCharacters = movie.characters;
 
+  // For each url in the list of characters print the name of the character
   urlsCharacters.forEach((characterUrl) => {
     request(characterUrl, (err, res, charBody) => {
       if (!err) {
