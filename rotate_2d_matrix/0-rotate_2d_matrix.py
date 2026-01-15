9c4005fa6c62e3d64b2985f935c30f04d986b18c
vlldnt <vieilledent.adrien@gmail.com>
2026-01-15 09:17:07 +0000

    feat: refactor 2D matrix rotation function and add .gitignore

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 '''rotate a 2d matrix 90 degrees clockwise in-place'''
 
+import numpy as np
+
 
 def rotate_2d_matrix(matrix):
-    '''Rotate a 3x3 2D matrix 90 degrees clockwise in-place'''
-    # original values
-    a, b, c = matrix[0][0], matrix[0][1], matrix[0][2]
-    d, e, f = matrix[1][0], matrix[1][1], matrix[1][2]
-    g, h, i = matrix[2][0], matrix[2][1], matrix[2][2]
-
-    # rotated values
-    matrix[0][0], matrix[0][1], matrix[0][2] = g, d, a
-    matrix[1][0], matrix[1][1], matrix[1][2] = h, e, b
-    matrix[2][0], matrix[2][1], matrix[2][2] = i, f, c
+    n = len(matrix)
+    rotated = np.rot90(matrix, -1)
+    for i in range(n):
+        for j in range(n):
+            matrix[i][j] = int(rotated[i][j])
