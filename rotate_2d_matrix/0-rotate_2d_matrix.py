828c7452f965b02b55f749139684aa475e693c1a
vlldnt <vieilledent.adrien@gmail.com>
2026-01-15 09:24:50 +0000

    feat: optimize 2D matrix rotation by removing numpy dependency

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 '''rotate a 2d matrix 90 degrees clockwise in-place'''
 
-import numpy as np
-
 
 def rotate_2d_matrix(matrix):
-    n = len(matrix)
-    rotated = np.rot90(matrix, -1)
-    for i in range(n):
-        for j in range(n):
-            matrix[i][j] = int(rotated[i][j])
+    reversed_matrix = matrix[::-1]
+    rotated = zip(*reversed_matrix)
+    matrix[:] = [list(row) for row in rotated]
