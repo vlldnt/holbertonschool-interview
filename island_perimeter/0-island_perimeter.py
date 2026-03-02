534477d3c48544a305204fdf5d10d6bd20b664bb
vlldnt <vieilledent.adrien@gmail.com>
2026-03-02 11:13:05 +0100

    feat: calclulate perimete rwithout index error

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-'''Calculate the pertimeter of an insland given in the grid'''
+"""Calculate the perimeter of an island given in the grid"""
 
 
 def island_perimeter(grid):
-    '''Fucntion to calculate the perimeter of an inland of 1 '''
+    """Return the perimeter of the island described in grid"""
     if not grid:
-        return None
+        return 0
 
     size = len(grid)
     perimeter = 0
@@ -13,21 +13,12 @@ def island_perimeter(grid):
     for i in range(size):
         for j in range(size):
             if grid[i][j] == 1:
-                # Init perimeter to 4 for on case (4 sides)
                 perimeter += 4
-                # check if top value == 1
-                if grid[i + 1][j] == 1:
-                    perimeter -= 1
+                # Check the top case
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                # Check the left case
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
 
-                # check if bottom value == 1
-                if grid[i - 1][j] == 1:
-                    perimeter -= 1
-
-                # check if right value == 1
-                if grid[i][j + 1] == 1:
-                    perimeter -= 1
-
-                # check if left value == 1
-                if grid[i][j - 1] == 1:
-                    perimeter -= 1
     return perimeter
