631eab6eee10b6e8142a72c37dbed0cb2d0565de
vlldnt <vieilledent.adrien@gmail.com>
2026-04-02 13:08:02 +0200

    feat: add Pascal Triangle implementation and main function

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+    Function that returns a list of listss of integer 
+    représentatuin the pascale truengle of n
+'''
+
+
+def pascal_triangle(n):
+    '''return a Pascal Triangle in list of lists'''
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        row = [1] * (i + 1)  # Starts with a 1 and end with a 1
+
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+
+        triangle.append(row)
+
+    return triangle
