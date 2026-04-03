425a70c91bb86f06e4ab21945e2573af0ba41b92
vlldnt <vieilledent.adrien@gmail.com>
2026-04-03 10:00:36 +0200

    feat: enhance regex matching with additional test cases and improved logic

--- regex/main.c
+++ regex/main.c
@@ -16,6 +16,57 @@
  */
 int main(void)
 {
+	TEST_MATCH("A", "A");
+	TEST_MATCH("A", "");
+	TEST_MATCH("", "A");
+	TEST_MATCH("", "");
+	TEST_MATCH("AA", "A");
+	TEST_MATCH("A", "AA");
+
+	TEST_MATCH("A", "A*");
+	TEST_MATCH("AA", "A*");
+	TEST_MATCH("AAAAA", "A*");
+	TEST_MATCH("AAAAAAAAAA", "A*");
+	TEST_MATCH("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", "A*");
+	TEST_MATCH("", "A*");
+	TEST_MATCH("B", "A*");
+	TEST_MATCH("AB", "A*");
+	TEST_MATCH("BA", "A*");
+
+	TEST_MATCH("AB", "AB");
+	TEST_MATCH("AB", "AB*");
+	TEST_MATCH("ABB", "AB*");
+	TEST_MATCH("A", "AB*");
+	TEST_MATCH("A", "ABCDEFGHIJKLMNOPQRSTUVWXYZ*");
+	TEST_MATCH("AZ", "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+	TEST_MATCH("AZ", "ABCDEFGHIJKLMNOPQRSTUVWXYZ*");
+	TEST_MATCH("AFJLOSWZ", "ABCDEFGHIJKLMNOPQRSTUVWXYZ*");
+	TEST_MATCH("AFJPLOSWZ", "ABCDEFGHIJKLMNOPQRSTUVWXYZ*");
+
+	TEST_MATCH("AB", "A.");
+	TEST_MATCH("ABC", "A.C");
+	TEST_MATCH("ABC", "A..");
+	TEST_MATCH("ABC", "A.");
+	TEST_MATCH("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "A...................");
+	TEST_MATCH("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "A..................Z");
+	TEST_MATCH("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "A.................Z");
+
+	TEST_MATCH("", ".");
+	TEST_MATCH("", ".*");
+	TEST_MATCH("A", ".*");
+	TEST_MATCH("AA", ".*");
+	TEST_MATCH("AABB", ".*");
+	TEST_MATCH("Lorem ipsum dolor si amet", ".*");
+
+	TEST_MATCH("ABC", "ZA.");
+	TEST_MATCH("ABC", "ZAB.");
+	TEST_MATCH("ABC", "ZABC.");
+	TEST_MATCH("ABC", "Z.");
+	TEST_MATCH("ABC", ".*");
+	TEST_MATCH("ABC", "..");
+	TEST_MATCH("ABC", "....................");
+	TEST_MATCH("", "....................");
+
 	TEST_MATCH("H", "H");
 	TEST_MATCH("HH", "H");
 	TEST_MATCH("HH", "H*");
@@ -30,11 +81,24 @@ int main(void)
 	TEST_MATCH("Holberton", "Z*H.*olberton");
 	TEST_MATCH("Holberton", "Z*H.*o.");
 	TEST_MATCH("Holberton", "Z*H.*o");
-
 	TEST_MATCH("Holberton", "holberton");
 	TEST_MATCH("Holberton", ".olberton");
 
-	TEST_MATCH("!H@o#l$b%e^r&t(o)n_", "!.@.#.$.%.^.&.(*");
+	TEST_MATCH("Lorem ipsum dolor si amet", "Lorem ipsum dolor si amet");
+	TEST_MATCH("Lorem ipsum dolor si amet", "Lor.*m ipsum dolor si amet");
+	TEST_MATCH("Lorem ipsum dolor si amet", "Lore.*m ipsum dolor si amet");
+	TEST_MATCH("Lorem ipsum dolor si amet", ".*Lorem ipsum dolor si amet");
+	TEST_MATCH("Lorem ipsum dolor si amet", "Lorem ipsum dolor si amet.*");
+	TEST_MATCH("Lorem ipsum dolor si amet", ".Lorem ipsum dolor si amet.");
+	TEST_MATCH("Lorem ipsum dolor si amet", ".Lorem..ipsum dolor si amet.*");
+	TEST_MATCH("Lorem ipsum dolor si amet", ".Lorem.ipsum dolor si amet.*");
+	TEST_MATCH("Lorem ipsum dolor si amet", ".Lorem ipsum dolor si amet.");
+	TEST_MATCH("Lorem ipsum dolor si amet", ".Lorem .* dolor si amet.");
+	TEST_MATCH("Lorem ipsum dolor si amet", ".Lorem ... dolor si amet.");
+	TEST_MATCH("Lorem ipsum dolor si amet", ".orem ipsum dolor si amet");
+	TEST_MATCH("Lorem ipsum dolor si amet", "Lorem ipsum dolor si ame.");
+
+	TEST_MATCH("!H@o#l$b%e^r&t(o)n_", "!.@.#.$.%.^.&.(.)._");
 
 	return (EXIT_SUCCESS);
 }
--- regex/regex.c
+++ regex/regex.c
@@ -1,46 +1,29 @@
-#include "stdio.h"
-#include <regex.h>
+#include "regex.h"
+
 /**
  * regex_match - checks if a string matches a regex pattern
  * @str: the string to check
- * @pattern: the pattern to match against
- * (supports '.'  one and '*' many carachters
+ * @pattern: the pattern to match against (supports '.' and '*')
+ *
+ * The matching rules are:
+ *   '.' matches any single character
+ *   '*' matches zero or more occurrences of the preceding character
  *
  * Return: 1 if @str matches @pattern, 0 otherwise
  */
-
 int regex_match(char const *str, char const *pattern)
 {
-	const char *star = NULL;
-	const char *match = str;
+	int first_match;
 
-	while (*str)
-	{
-		if (*str == *pattern || *pattern == '.')
-		{
-			str++;
-			pattern++;
-		}
-		else if (*pattern == '*')
-		{
-			/* wildcard found: save its position and current str */
-			star = pattern;
-			match = str;
-			pattern++;
-		}
-		else if (star)
-		{
-			/* mismatch but a previous '*' exists: backtrack */
-			pattern = star + 1;
-			match++;
-			str = match;
-		}
-		else
-			return (0);
-	}
-	/* skip any trailing stars in the pattern */
-	while (*pattern == '*')
-		pattern++;
+	if (*pattern == '\0')
+		return (*str == '\0');
 
-	return (*pattern == '\0');
+	first_match = (*str != '\0') &&
+		(*pattern == '.' || *pattern == *str);
+
+	if (*(pattern + 1) == '*')
+		return (regex_match(str, pattern + 2) ||
+			(first_match && regex_match(str + 1, pattern)));
+
+	return (first_match && regex_match(str + 1, pattern + 1));
 }
