0148ee7c56103089f3b673b18d536704c5aafe68
vlldnt <vieilledent.adrien@gmail.com>
2026-04-03 14:15:51 +0200

    feat: implement prime game logic and add main execution script

---
+++ primegame/0-prime_game.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Prime Game"""
+
+
+def isWinner(x, nums):
+    """Determine who wins the most rounds of the prime game."""
+    if not nums or x == 0:
+        return None
+
+    max_n = max(nums)
+
+    # Sieve of Eratosthenes
+    sieve = [True] * (max_n + 1)
+    sieve[0] = sieve[1] = False
+    for i in range(2, int(max_n ** 0.5) + 1):
+        if sieve[i]:
+            for j in range(i * i, max_n + 1, i):
+                sieve[j] = False
+
+    # Prefix count of primes up to index i
+    prime_count = [0] * (max_n + 1)
+    for i in range(1, max_n + 1):
+        prime_count[i] = prime_count[i - 1] + (1 if sieve[i] else 0)
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums[:x]:
+        if prime_count[n] % 2 == 1:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    if ben_wins > maria_wins:
+        return "Ben"
+    return None
